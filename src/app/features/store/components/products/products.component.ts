9cd12e396dba16be3a51f855f609f09642654c32
Antonella <antonellatorrez20@gmail.com>
2024-12-22 20:40:07 -0400

    filtros_categoria_precio

--- src/app/features/store/components/products/products.component.ts
+++ src/app/features/store/components/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductsService } from '../../../../core/services/products/products.ser
 import { product } from '../../../../core/model/product.model';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { machine } from 'node:os';
 
 @Component({
   selector: 'app-products',
@@ -14,12 +15,17 @@ import { NgxPaginationModule } from 'ngx-pagination';
 export class ProductsComponent implements OnInit {
   currentPage = 1
   products: product[] = []
-  constructor(private productsService: ProductsService){}
+  filteredProducts: product[] = []
+  selectedCategory: string = 'all'
+  maxPrice: number = 100
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
     this.productsService.getProducts().subscribe(
-      (data:product[]) => {
-        this.products=data
+      (data: product[]) => {
+        this.products = data
+        this.filteredProducts = data.slice()
+        this.applyFilters()
       }
     )
   }
@@ -31,5 +37,29 @@ export class ProductsComponent implements OnInit {
 
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  filterProducts(category: string): void {
+    this.selectedCategory = category
+    console.log("categoria", this.selectedCategory)
+    this.applyFilters()
+  }
+
+  productsByPrice(event: Event): void {
+    const target = event.target as HTMLInputElement | null
+    if (target) {
+      this.maxPrice = Number(target.value)
+      console.log("precio", this.maxPrice)
+      this.applyFilters()
+    }
+  }
+
+  applyFilters(): void {
+    this.filteredProducts = this.products.filter(product => {
+      const matchesCategory = (product.category === this.selectedCategory) || this.selectedCategory === 'all'
+      const matchesPrice = product.price <= this.maxPrice
+      return matchesCategory && matchesPrice
+    })
+    this.currentPage = 1
   }
 }
