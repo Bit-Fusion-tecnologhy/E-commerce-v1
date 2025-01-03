b2745a4fa6adb865a0630e93ca03f98ad7e4d0bf
Fernando <fcamata77@gmail.com>
2025-01-02 22:28:31 -0400

    reparacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { ComponentsComponent } from './features/store/components/components.comp
 import { AdminComponent } from './features/dashboard/admin/admin.component';
 import { HomeComponent } from './features/store/components/home/home.component';
 import { ProductsComponent } from './features/store/components/products/products.component';
+import { DetailStoreComponent } from './features/store/components/detail-store/detail-store.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/store', pathMatch: 'full' },
     { path: 'store', component: ComponentsComponent, children:[
         {path:'', redirectTo:'home', pathMatch:'full'},
         {path:'home',component:HomeComponent},
-        {path:'products', component:ProductsComponent}
+        {path:'products', component:ProductsComponent},
+        {path:'detail', component:DetailStoreComponent}
     ] },
     { path: 'dashboard', component: AdminComponent }
   ];
--- src/app/features/store/components/home/home.component.ts
+++ src/app/features/store/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductsService } from '../../../../core/services/products/products.service';
 import { TruncatePipe } from "../../../shared/pipes/home/truncate.pipe";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,7 @@ export class HomeComponent {
   listProducts: any[] = [];
   listProductsView: any[] = [];
   
-  constructor(private productService: ProductsService) { }
+  constructor(private productService: ProductsService, private router: Router) { }
   
   ngOnInit(): void {
     this.productService.getProducts().subscribe((xdat) => (
@@ -22,4 +23,9 @@ export class HomeComponent {
       this.listProductsView = this.listProducts.slice(0, 6)
     ))
   }
+
+  navegar() {
+    this.router.navigate(['detail'], { relativeTo: this.router.routerState.root.firstChild });
+  }
+  
 }
